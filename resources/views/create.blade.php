1ca175a1a75addaa5f6b09374fe58be07bf56818
IgorGarciaCosta <igonildo7@gmail.com>
2020-08-24 10:41:32 -0300

    editMethod

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,7 +1,10 @@
 @extends('templates.template')
 
 @section('content')
-<h1 class="text-center">Cadastrar</h1>
+
+<!-- //escreve Editar ou Cadastar a depender da entrada 
+//que recebe da edit() do bookCOntroller -->
+<h1 class="text-center">@if(isset($book)) Editar @else Cadastar @endif</h1>
 <hr>
 
 <!-- @csrf serve pra impedir ataques de cross-
@@ -9,35 +12,41 @@ site. (é um token). -->
 <div class="col-8 m-auto">
 
     @if(isset($errors)&& count($errors)>0)
-        <div class="text-center mt-4 mb-4 p-2 alert-danger">
-            @foreach($errors->all() as $erro)
-                {{$erro}}
-            @endforeach
-        </div>
+    <div class="text-center mt-4 mb-4 p-2 alert-danger">
+        @foreach($errors->all() as $erro)
+        {{$erro}}
+        @endforeach
+    </div>
     @endif
 
 
-    <form name="formCad" id="formCad" method="post" action="{{url('books')}}">
-        @csrf
+    @if(isset($book))
+        <form name="formEdit" id="formEdit" method="post" action="{{url("books/$book->id")}}">
+            @method('PUT')
+     @else
 
-        <!-- Obs:input faz ter as textBoxes. 
-            placeholder é o texto dentra das textBoxes
-            que some quando começamos a digitar. -->
-        <input class="form-control" type="text" name="title" id="title" placeholder="Título" required >
-        <br>
-        <select class="form-control" name="id_user" id="id_user" >
-            <option value="">Autor</option>
-            @foreach($users as $user)
-            <option value="{{$user->id}}">{{$user->name}}</option>
+        <form name="formCad" id="formCad" method="post" action="{{url('books')}}">
+    @endif
+    @csrf
 
-            @endforeach
-        </select>
-        <br>
-        <input class="form-control" type="text" name="pages" id="pages" placeholder="Páginas"required >
-        <br>
-        <input class="form-control" type="text" name="price" id="title" placeholder="Preço" required>
-        <br>
-        <input class="btn btn-primary" type="submit" value="Cadastrar">
+    <!-- Obs:input faz ter as textBoxes. 
+    placeholder é o texto dentra das textBoxes
+    que some quando começamos a digitar. -->
+    <input class="form-control" type="text" name="title" id="title" placeholder="Título" value = "{{$book->title ?? ''}}" required>
+    <br>
+    <select class="form-control" name="id_user" id="id_user">
+        <option value="{{$book->relUsers->id?? ''}}">{{$book->relUsers->name ?? 'Autor'}}</option>
+        @foreach($users as $user)
+        <option value="{{$user->id}}">{{$user->name}}</option>
+
+        @endforeach
+    </select>
+    <br>
+    <input class="form-control" type="text" name="pages" id="pages" placeholder="Páginas" value = "{{$book->pages ?? ''}}" required>
+    <br>
+    <input class="form-control" type="text" name="price" id="title" placeholder="Preço" value = "{{$book->price ?? ''}}" required>
+    <br>
+    <input class="btn btn-primary" type="submit" value="@if(isset($book)) Editar @else Cadastar @endif">
 
     </form>
 
